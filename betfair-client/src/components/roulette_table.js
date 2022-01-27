8a095c40852fd681b2b2d11b3da91485c9fd2457
coingod4 <coingod4@gmail.com>
2022-01-26 23:29:14 -0500

    added all betting buttons

--- betfair-client/src/components/roulette_table.js
+++ betfair-client/src/components/roulette_table.js
@@ -4,13 +4,14 @@
  * This defines a react component containing all roulette table graphics & features.
  */
 
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, autocompleteClasses } from "@mui/material";
 import { Box } from "@mui/system";
 import { Wheel } from "react-custom-roulette";
 import { ROULETTE_WHEEL, ROULETTE_WHEEL_COLOURS } from "../utils/roulette_config";
 import Chat from "./chat";
 
 function RouletteTable(props) {
+    // Used as input for the react-custom-roulette wheel component
     let wheelData = [];
     for (let num of ROULETTE_WHEEL) {
         wheelData.push({
@@ -19,6 +20,7 @@ function RouletteTable(props) {
         });
     }
 
+    // Generate an array of vertically-arrange 3-button groups
     let bettingButtons = [];
     for (let num = 1; num <= ROULETTE_WHEEL.length; num += 3) {
         let buttons = [];
@@ -27,7 +29,6 @@ function RouletteTable(props) {
             buttons.push(
                 <Box 
                     sx={{
-                        width: 75,
                         height: 75,
                         backgroundColor: ROULETTE_WHEEL_COLOURS[currNum],
                     }}
@@ -39,6 +40,46 @@ function RouletteTable(props) {
             <div className="roulette-button-group">{buttons}</div>
         );
     }
+
+    // Add 3 button group to end of betting buttons array containing row bets
+    bettingButtons.push(
+        <div className="roulette-button-group wide">
+            <Box key="first-row" sx={{height: 75, backgroundColor: "#808080"}}className="roulette-number-box">2:1</Box>
+            <Box key="second-row" sx={{height: 75, backgroundColor: "#808080"}}className="roulette-number-box">2:1</Box>
+            <Box key="third-row" sx={{height: 75, backgroundColor: "#808080"}}className="roulette-number-box">2:1</Box>
+        </div>
+    );
+
+    // Add additional betting buttons below numbers and row selectors
+    let numRangeSelectors = [
+        <Box sx={{width: "30%", height: 50, backgroundColor: "#808080"}}className="roulette-number-box">1st 12</Box>,
+        <Box sx={{width: "30%", height: 50, backgroundColor: "#808080"}}className="roulette-number-box">2nd 12</Box>,
+        <Box sx={{width: "30%", height: 50, backgroundColor: "#808080"}}className="roulette-number-box">3rd 12</Box>
+    ];
+
+    let evenSelectors = [
+        <Box sx={{height: 50, backgroundColor: "#808080"}}className="roulette-number-box even-selector">0-18</Box>,
+        <Box sx={{height: 50, backgroundColor: "#808080"}}className="roulette-number-box even-selector">Even</Box>,
+        <Box sx={{height: 50, backgroundColor: "#DAA520"}}className="roulette-number-box even-selector">Gold</Box>,
+        <Box sx={{height: 50, backgroundColor: "#000000"}}className="roulette-number-box even-selector">Black</Box>,
+        <Box sx={{height: 50, backgroundColor: "#808080"}}className="roulette-number-box even-selector">Odd</Box>,
+        <Box sx={{height: 50, backgroundColor: "#808080"}}className="roulette-number-box even-selector">19-36</Box>
+    ]
+    let bettingZone = (
+        <div id="betting-zone">
+            <div id="roulette-numbers">
+                {bettingButtons}
+            </div>
+            <div className="d-flex flex-row">
+                {numRangeSelectors}
+            </div>
+            <div className="d-flex flex-row">
+                {evenSelectors}
+            </div>
+        </div>
+
+    );
+
     return (
         <Grid container>
             <Grid item xs={9}>
@@ -54,11 +95,7 @@ function RouletteTable(props) {
                             outerBorderWidth={2}
                         />
                     </div>
-                    <div id="betting-zone">
-                        <div id="roulette-numbers">
-                            {bettingButtons}
-                        </div>
-                    </div>
+                    {bettingZone}
                 </div>
             </Grid>
             <Grid item xs={3}>
